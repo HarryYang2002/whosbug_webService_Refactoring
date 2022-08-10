e51652cdd67aaa1b20d01189da356a590e10b754
1260828355 <1260828355@qq.com>
2022-08-10 22:37:01 +0800

    createUser.go 重构完全

--- views/createUser.go
+++ views/createUser.go
@@ -1,9 +1,13 @@
 package views
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"net/http"
 	. "webService_Refactoring/modules"
 )
@@ -24,13 +28,33 @@ func UserCreate(context *gin.Context) {
 	tokenKey = registerForm.Username + "&" + registerForm.Password
 	token := MD5(tokenKey)
 
+	var userUUID uuid.UUID
+	userUUID = CreateUUID()
+
 	context.JSON(http.StatusOK, gin.H{
-		"id":           "", //数据库中的id
-		"username":     registerForm.Username,
-		"password":     registerForm.Password,
-		"first_name":   registerForm.Firstname,
-		"last_name":    registerForm.Lastname,
-		"email":        registerForm.Email,
-		"authon_token": token,
+		"id":         userUUID.String(),
+		"username":   registerForm.Username,
+		"password":   registerForm.Password,
+		"first_name": registerForm.Firstname,
+		"last_name":  registerForm.Lastname,
+		"email":      registerForm.Email,
+		"auth_token": token,
 	})
+
+	dbCreateUser := DbCreateUser{
+		UserId:        userUUID,
+		UserName:      registerForm.Username,
+		UserToken:     token,
+		UserPassword:  registerForm.Password,
+		UserFirstName: registerForm.Firstname,
+		UserLastName:  registerForm.Lastname,
+		UserEmail:     registerForm.Email,
+	}
+	dsn := "host=localhost user=postgres password=123456 dbname=whobug2022 port=5433 " +
+		"sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		err.Error()
+	}
+	fmt.Println(db.Table("users").Create(&dbCreateUser).RowsAffected)
 }
